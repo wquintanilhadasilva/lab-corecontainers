a6a706fcd667123a13ad6e4fce918d16f914360b
Wedson Quintanilha da Silva <wquintanilhadasilva@gmail.com>
2021-03-01 20:10:31 -0300

    initial commit

---
+++ lab-corecontainers/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab_corecontainers.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public object Get([FromServices] IConfiguration configuration)
+        {
+            return new
+            {
+                Local = Environment.MachineName,
+                Mensagem = configuration["MENSAGEM"]
+            };
+        }
+    }
+}
